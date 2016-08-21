195510b4dde84114bc877d937399d1d05664d9c8
rahul2u <rsembedded@gmail.com>
2016-08-21 17:48:49 +0530

    My First Commit..

---
+++ dataframe.r
@@ -0,0 +1,8 @@
+# This is Demo of Creating the Data Frame with Name function
+
+defect.counts <- c(12,29,18,3,34,4)
+names(defect.counts) <- c("Weather","Overslept","Alarm Failure","Time Change","Traffic","Other")
+defect.counts
+
+df.defects <- data.frame(defect.counts)
+df.defects
